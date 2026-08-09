ae898b360c73740c6b9596e9a7f67b6149422eb4
jonathan <mokoko@MacBook-Pro-de-jonathan.local>
2026-08-09 19:42:26 +0100

    feat(backend): route de connexion (login) avec JWT et verification bcrypt

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const prisma = require('../config/prisma');
+
+
+
+async function login ( req , res ) {
+
+    const { email , password } = req.body ; 
+
+    const user = await prisma.user.findUnique ({ where : { email } });
+
+    if (!user){
+        return res.status(401).json ({
+            message : 'email ou mot de passe incorrect '
+        });
+    }
+
+    const passwordMatches = await bcrypt.compare(password,user.password);
+
+    if (!passwordMatches){
+        return res.status(401).json({
+            message : 'mot de passe incorrect'
+        });
+    }
+
+    const token = jwt.sign ( 
+        { userId: user.id , role : user.role },
+        process.env.JWT_SECRET,{ expiresIn : '8h'}
+    );
+
+    res.json({
+        token,
+        user:{
+            id : user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        },
+    });
+
+}
+
+module.exports = { login } ;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5003 ;
+app.use(express.json());
 
 app.get('/api/health', (req, res) => {
     res.json({ status: 'ok' });
     console.log("l'api est en vie ");
 });
+
+app.use('/api/auth',authRoutes);
 
 app.listen(PORT, () => {
     console.log(`server démarré sur http://localhost:${PORT}/api/health`);
