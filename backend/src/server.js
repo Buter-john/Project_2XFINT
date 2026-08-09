63731da2bf786af9eba5880b966911a4c564584a
jonathan <mokoko@MacBook-Pro-de-jonathan.local>
2026-08-09 18:23:47 +0100

    feat(backend): serveur Express minimal avec route health check

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003 ;
 
 app.get('/api/health', (req, res) => {
     res.json({ status: 'ok' });
+    console.log("l'api est en vie ");
 });
 
 app.listen(PORT, () => {
-    console.log(`server démarré sur http://localhost:${PORT}`);
+    console.log(`server démarré sur http://localhost:${PORT}/api/health`);
 });
 
